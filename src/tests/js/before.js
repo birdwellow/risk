1766a2df7e9cd783270ff0876e5ffe75d5eb29f4
birdwellow <contact@fvogel.net>
2016-05-12 20:54:40 +0200

    Add first unit tests

--- src/tests/js/before.js
+++ src/tests/js/before.js
@@ -1,3 +1,35 @@
-console.log("before");
-
 var joinId = 'abcd1234';
+
+/*
+ * Custom mock for WebSocket
+ */
+function WebSocket () {
+	
+	var eventListners = {};
+	WebSocket.messages = [];
+	
+	this.addEventListener = function (eventName, handler){
+		if(!eventListners[eventName]){
+			eventListners[eventName] = [];
+		}
+		eventListners[eventName].push(handler);
+	};
+	
+	this.send = function (messageString) {
+		console.log('Sent with mock WebSocket: ' + messageString);
+		WebSocket.messages.push(messageString);
+	};
+	
+	WebSocket._receive = function (eventName, eventObject) {
+		if(eventListners[eventName]){
+			var handlers = eventListners[eventName];
+			for (var i in handlers) {
+				handlers[i](eventObject);
+			}
+		}
+	};
+	
+	WebSocket._lastMessage = function () {
+		return WebSocket.messages[WebSocket.messages.length - 1];
+	};
+}
--- src/tests/js/unit/ProxyTestSpec.js
+++ src/tests/js/unit/ProxyTestSpec.js
@@ -1,8 +1,37 @@
-describe('someTestSpec', function () {
+describe('Proxy', function () {
 	
-	it('works', function () {
+	it('can send data', function () {
 		
-		expect(3).toBe(3);
+		var proxy =
+				new CommunicationProxy("ws://ran.dom.host:1337/?joinid=abcd1234");
+		
+		proxy.send('some.event', {
+			string: 'data',
+			array: ['d', 'a', 't', 'a'],
+			object: {
+				key: 'value'
+			}
+		});
+		
+		expect(WebSocket._lastMessage()).toBe('{"type":"some.event","data":{"string":"data","array":{"0":"d","1":"a","2":"t","3":"a"},"object":{"key":"value"}}}');
+		
+	});
+	
+	it('can receive data', function () {
+		
+		var proxy =
+				new CommunicationProxy("ws://ran.dom.host:1337/?joinid=abcd1234");
+		
+		proxy.send('some.event', {
+			string: 'data',
+			array: ['d', 'a', 't', 'a'],
+			object: {
+				key: 'value'
+			}
+		});
+		
+		expect(WebSocket._lastMessage())
+				.toBe('{"type":"some.event","data":{"string":"data","array":{"0":"d","1":"a","2":"t","3":"a"},"object":{"key":"value"}}}');
 		
 	});
 	
